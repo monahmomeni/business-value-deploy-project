3558cc617756255f7367400bd795c5c2cd2d3e21
monahmomeni <floweryaspie@gmail.com>
2020-11-19 15:23:07 -0500

    second unzip for data files

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 kaggle competitions download -c predicting-red-hat-business-value -p .
-unzip predicting-red-hat-business-value.zip -d packages/lr_customer_value/lr_customer_value/datasets/
+unzip predicting-red-hat-business-value.zip -d tmp
+unzip tmp/*.csv.zip -d packages/lr_customer_value/lr_customer_value/datasets/
