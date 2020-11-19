c78fa25b14191f1d20397614d42bb9aa46ab1e6b
monahmomeni <floweryaspie@gmail.com>
2020-11-18 22:52:39 -0500

    tracking project on circleci

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 kaggle competitions download -c predicting-red-hat-business-value -p .
 unzip predicting-red-hat-business-value.zip -d packages/lr_customer_value/lr_customer_value/datasets/
