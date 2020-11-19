95b4bad3e9cbba422ee207d40aa767d0ecf4b75d
monahmomeni <floweryaspie@gmail.com>
2020-11-19 15:35:57 -0500

    fixed bug in unzip

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,4 +2,4 @@
 
 kaggle competitions download -c predicting-red-hat-business-value -p .
 unzip predicting-red-hat-business-value.zip -d tmp
-unzip tmp/*.csv.zip -d packages/lr_customer_value/lr_customer_value/datasets/
+unzip 'tmp/*.csv.zip' -d packages/lr_customer_value/lr_customer_value/datasets/
